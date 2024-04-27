1a550bdd512e5dec2cae634614fec1f61a0f0098
maryamsartaj <168002522+maryamsartaj@users.noreply.github.com>
2024-04-28 01:49:38 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,12 @@
+// Creating a function with parameters which return a values in strin
+function city_country(city: string, country: string) : string{
+    return `${city} , ${country}`;
+}
+
+// Calling a function and print the returned values
+
+console.log(city_country("Karachi" , "Pakistan"));
+
+console.log(city_country("Tokyo" , "Japan"));
+
+console.log(city_country("Berlin" , "Germany"));
